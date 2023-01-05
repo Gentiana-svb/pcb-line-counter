13158324c48a81645ff96619c26119580c130656
Gentiana-svb <rowosmar31415@icloud.com>
2023-01-05 17:53:43 +0900

    fix: trailing slash settings

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,6 @@ const config = {
     adapter: adapter({
       fallback: 'index.html'
     }),
-    trailingSlash: 'always',
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/pcb-line-counter' : ''
     }
