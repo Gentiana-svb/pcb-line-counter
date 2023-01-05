1a9ea3045b447de5eab61cfd72d9c4f2d8ca4e36
Gentiana-svb <rowosmar31415@icloud.com>
2023-01-05 17:34:07 +0900

    ref page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,20 @@
 
   let imgSrc = ''
   let imgElement: HTMLImageElement
-  let precessing = false
+  let inputElement: HTMLInputElement | null
+  let processing = false
+  let redCount = 0
+  let blueCount = 0
 
   const fileLoad = (files: FileList | undefined | null) => {
     if (files?.length) {
-      precessing = true
+      processing = true
       imgSrc = URL.createObjectURL(files[0])
     }
+  }
+
+  const clearSelectedFile = () => {
+    if (inputElement) inputElement.value = ''
   }
 
   const countColor = (vectL: number[], vectH: number[]) => {
@@ -43,21 +50,42 @@
   const imgLoad = () => {
     redCount = countColor([200, 0, 0, 0], [255, 0, 0, 255])
     blueCount = countColor([0, 0, 200, 0], [0, 0, 255, 255])
-    precessing = false
+    processing = false
   }
 
-  let redCount = 0
-  let blueCount = 0
+  const onDrop = (e: DragEvent) => {
+    clearSelectedFile()
+    fileLoad(e.dataTransfer?.files)
+  }
+
+  const onPaste = async () => {
+    processing = true
+
+    const data = await navigator.clipboard.read()
+    const d = data[0]
+
+    if (!d?.types.includes('image/png')) {
+      alert('No available image in clipboard')
+      processing = false
+      return
+    }
+
+    clearSelectedFile()
+    const b = await d.getType('image/png')
+    const blob = new Blob([b], { type: 'image/png' })
+
+    imgSrc = URL.createObjectURL(blob)
+  }
 </script>
 
 <main
   on:dragover|preventDefault
   on:dragleave|preventDefault
-  on:drop|preventDefault={e => fileLoad(e.dataTransfer?.files)}
+  on:drop|preventDefault={onDrop}
   class="h-screen w-screen flex flex-col items-center justify-center"
 >
   {#if imgSrc}
-    {#if precessing}
+    {#if processing}
       <div class="text-3xl font-bold m-4">Loading...</div>
     {:else}
       <div class="grid grid-auto-2 text-3xl font-bold gap-2 m-4">
@@ -69,17 +97,21 @@
     {/if}
   {/if}
   <div class="grid grid-auto-3 items-center gap-3 m-3">
-    <button class="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white rounded px-3 py-2">
+    <button
+      class="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white rounded px-3 py-2"
+      on:click={onPaste}
+    >
       Paste image
     </button>
     <label
-      class="px-3 py-2 rounded cursor-pointer bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white"
+      class="px-3 py-2 rounded cursor-pointer border border-gray-800  hover:bg-gray-100 active:bg-gray-200"
     >
       <input
         type="file"
         accept="image/*"
         on:change={e => fileLoad(e.currentTarget?.files)}
         class="hidden"
+        bind:this={inputElement}
       />
       Select image
     </label>
